2ce8661c4df0ca66540702b5687c24c783c7aa75
Brian Sakurada <sakuradabs@gmail.com>
2019-09-19 16:45:09 +0900

    Added my name to the footer.

--- themes/gaijin-assist/footer.php
+++ themes/gaijin-assist/footer.php
@@ -15,16 +15,9 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'gaijin-assist' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'gaijin-assist' ), 'WordPress' );
-				?>
-			</a>
 			<span class="sep"> | </span>
 				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'gaijin-assist' ), 'gaijin-assist', '<a href="http://underscores.me/">Underscores.me</a>' );
+				printf( esc_html__( 'Created by Brian Sakurada 2019' ));
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
